959519d81fa6a65d33155c4dc534355813e3d8dc
EstebanPechuan <esteban.pechuan@gmail.com>
2022-04-13 00:30:11 -0300

    Trabajos realizados

--- app/Views/exitos.php
+++ app/Views/exitos.php
@@ -6,9 +6,9 @@
     <div class="casos">
         <div class="cada-trabajo">
             <div class="trabajo-img">
-                <img src=" <?php echo base_url('public/img/exito1.png') ?> " alt="">
+                <img src=" <?php echo base_url('public/img/trabajos-realizados/de-que-se-habla.png') ?> " alt="">
             </div>
-            <h4>Nombre del proyecto</h4>
+            <h4>De que se habla</h4>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias veniam hic harum ipsum, consequatur ut enim beatae laboriosam excepturi fugiat, ducimus rerum corporis explicabo labore odit quas? Reprehenderit, iste natus!</p>
             <a href="#c">Conocé más
                 <span>🡢</span>
@@ -17,9 +17,9 @@
 
         <div class="cada-trabajo">
             <div class="trabajo-img">
-                <img src=" <?php echo base_url('public/img/exito2.png') ?> " alt="">
+                <img src=" <?php echo base_url('public/img/trabajos-realizados/veladero.png') ?> " alt="">
             </div>
-            <h4>Nombre del proyecto</h4>
+            <h4>Veladero</h4>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias veniam hic harum ipsum, consequatur ut enim beatae laboriosam excepturi fugiat, ducimus rerum corporis explicabo labore odit quas? Reprehenderit, iste natus!</p>
             <a href="#c">Conocé más
                 <span>🡢</span>
@@ -28,9 +28,9 @@
 
         <div class="cada-trabajo">
             <div class="trabajo-img">
-                <img src=" <?php echo base_url('public/img/exito2.png') ?> " alt="">
+                <img src=" <?php echo base_url('public/img/trabajos-realizados/siap.png') ?> " alt="">
             </div>
-            <h4>Nombre del proyecto</h4>
+            <h4>SIAP</h4>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias veniam hic harum ipsum, consequatur ut enim beatae laboriosam excepturi fugiat, ducimus rerum corporis explicabo labore odit quas? Reprehenderit, iste natus!</p>
             <a href="#c">Conocé más
                 <span>🡢</span>
@@ -39,9 +39,9 @@
 
         <div class="cada-trabajo">
             <div class="trabajo-img">
-                <img src=" <?php echo base_url('public/img/exito1.png') ?> " alt="">
+                <img src=" <?php echo base_url('public/img/trabajos-realizados/habilitacion.png') ?> " alt="">
             </div>
-            <h4>Nombre del proyecto</h4>
+            <h4>Habilitación San Juan</h4>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias veniam hic harum ipsum, consequatur ut enim beatae laboriosam excepturi fugiat, ducimus rerum corporis explicabo labore odit quas? Reprehenderit, iste natus!</p>
             <a href="#c">Conocé más
                 <span>🡢</span>
@@ -50,9 +50,9 @@
 
         <div class="cada-trabajo">
             <div class="trabajo-img">
-                <img src=" <?php echo base_url('public/img/exito1.png') ?> " alt="">
+                <img src=" <?php echo base_url('public/img/trabajos-realizados/osse.png') ?> " alt="">
             </div>
-            <h4>Nombre del proyecto</h4>
+            <h4>OSSE San Juan</h4>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias veniam hic harum ipsum, consequatur ut enim beatae laboriosam excepturi fugiat, ducimus rerum corporis explicabo labore odit quas? Reprehenderit, iste natus!</p>
             <a href="#c">Conocé más
                 <span>🡢</span>
@@ -61,9 +61,97 @@
 
         <div class="cada-trabajo">
             <div class="trabajo-img">
-                <img src=" <?php echo base_url('public/img/exito2.png') ?> " alt="">
+                <img src=" <?php echo base_url('public/img/trabajos-realizados/cidi.png') ?> " alt="">
             </div>
-            <h4>Nombre del proyecto</h4>
+            <h4>Ciudadano Digital</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias veniam hic harum ipsum, consequatur ut enim beatae laboriosam excepturi fugiat, ducimus rerum corporis explicabo labore odit quas? Reprehenderit, iste natus!</p>
+            <a href="#c">Conocé más
+                <span>🡢</span>
+            </a>
+        </div>
+
+        <div class="cada-trabajo">
+            <div class="trabajo-img">
+                <img src=" <?php echo base_url('public/img/trabajos-realizados/cobros.png') ?> " alt="">
+            </div>
+            <h4>Cobros</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias veniam hic harum ipsum, consequatur ut enim beatae laboriosam excepturi fugiat, ducimus rerum corporis explicabo labore odit quas? Reprehenderit, iste natus!</p>
+            <a href="#c">Conocé más
+                <span>🡢</span>
+            </a>
+        </div>
+
+        <div class="cada-trabajo">
+            <div class="trabajo-img">
+                <img src=" <?php echo base_url('public/img/trabajos-realizados/app-educacion.png') ?> " alt="">
+            </div>
+            <h4>Nodos Educación</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias veniam hic harum ipsum, consequatur ut enim beatae laboriosam excepturi fugiat, ducimus rerum corporis explicabo labore odit quas? Reprehenderit, iste natus!</p>
+            <a href="#c">Conocé más
+                <span>🡢</span>
+            </a>
+        </div>
+
+        <div class="cada-trabajo">
+            <div class="trabajo-img">
+                <img src=" <?php echo base_url('public/img/trabajos-realizados/turnero.png') ?> " alt="">
+            </div>
+            <h4>Turnero</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias veniam hic harum ipsum, consequatur ut enim beatae laboriosam excepturi fugiat, ducimus rerum corporis explicabo labore odit quas? Reprehenderit, iste natus!</p>
+            <a href="#c">Conocé más
+                <span>🡢</span>
+            </a>
+        </div>
+
+        <div class="cada-trabajo">
+            <div class="trabajo-img">
+                <img src=" <?php echo base_url('public/img/trabajos-realizados/ciberseguridad.png') ?> " alt="">
+            </div>
+            <h4>Dirección de Ciberseguridad</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias veniam hic harum ipsum, consequatur ut enim beatae laboriosam excepturi fugiat, ducimus rerum corporis explicabo labore odit quas? Reprehenderit, iste natus!</p>
+            <a href="#c">Conocé más
+                <span>🡢</span>
+            </a>
+        </div>
+
+        <div class="cada-trabajo">
+            <div class="trabajo-img">
+                <img src=" <?php echo base_url('public/img/trabajos-realizados/dpi.png') ?> " alt="">
+            </div>
+            <h4>Dirección Provincial de Informática</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias veniam hic harum ipsum, consequatur ut enim beatae laboriosam excepturi fugiat, ducimus rerum corporis explicabo labore odit quas? Reprehenderit, iste natus!</p>
+            <a href="#c">Conocé más
+                <span>🡢</span>
+            </a>
+        </div>
+
+        <div class="cada-trabajo">
+            <div class="trabajo-img">
+                <img src=" <?php echo base_url('public/img/trabajos-realizados/dga.png') ?> " alt="">
+            </div>
+            <h4>Dirección de Gobierno Abierto</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias veniam hic harum ipsum, consequatur ut enim beatae laboriosam excepturi fugiat, ducimus rerum corporis explicabo labore odit quas? Reprehenderit, iste natus!</p>
+            <a href="#c">Conocé más
+                <span>🡢</span>
+            </a>
+        </div>
+
+        <div class="cada-trabajo">
+            <div class="trabajo-img">
+                <img src=" <?php echo base_url('public/img/trabajos-realizados/origen.png') ?> " alt="">
+            </div>
+            <h4>Origen San Juan</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias veniam hic harum ipsum, consequatur ut enim beatae laboriosam excepturi fugiat, ducimus rerum corporis explicabo labore odit quas? Reprehenderit, iste natus!</p>
+            <a href="#c">Conocé más
+                <span>🡢</span>
+            </a>
+        </div>
+
+        <div class="cada-trabajo">
+            <div class="trabajo-img">
+                <img src=" <?php echo base_url('public/img/trabajos-realizados/broker.png') ?> " alt="">
+            </div>
+            <h4>Broker Andina</h4>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias veniam hic harum ipsum, consequatur ut enim beatae laboriosam excepturi fugiat, ducimus rerum corporis explicabo labore odit quas? Reprehenderit, iste natus!</p>
             <a href="#c">Conocé más
                 <span>🡢</span>
